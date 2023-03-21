c6861f5d4266c3d81713ae61e1c1f631352c7c6c
Tran Viet Anh <tran@vietanh.cz>
2023-03-21 17:20:33 +0100

    Use <Heading /> in <Introduction />

--- components/landing-page/Introduction.tsx
+++ components/landing-page/Introduction.tsx
@@ -3,6 +3,7 @@ import { DocumentData } from 'firebase/firestore'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper.css'
 import ReactHtmlParser from 'react-html-parser'
+import { Heading } from './Heading'
 import styles from './Introduction.module.scss'
 
 interface Props {
@@ -11,22 +12,25 @@ interface Props {
 
 export default function Introduction ({ slidesData }: Props) {
   return (
-    <Swiper style={{height: '600px'}}>
-      {
-        slidesData && slidesData.map(slideData => (
-          <SwiperSlide style={{height: '600'}}>
-            <Container className={styles.textContainer}>
-              <h1>{ReactHtmlParser(slideData.title)}</h1>
-              <p>{ReactHtmlParser(slideData.text)}</p>
-            </Container>
-            <img
-              src={slideData.backgroundURL}
-              className={styles.slideImage}
-              alt={slideData.title}
-            />
-          </SwiperSlide>
-        ))
-      }
-    </Swiper>
+    <>
+      <Heading text='Kdo jsme'/>
+      <Swiper style={{ height: '600px' }}>
+        {
+          slidesData && slidesData.map(slideData => (
+            <SwiperSlide style={{ height: '600' }} key={slideData.id}>
+              <Container className={styles.textContainer}>
+                <h1>{ReactHtmlParser(slideData.title)}</h1>
+                <p>{ReactHtmlParser(slideData.text)}</p>
+              </Container>
+              <img
+                src={slideData.backgroundURL}
+                className={styles.slideImage}
+                alt={slideData.title}
+              />
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+    </>
   )
 }

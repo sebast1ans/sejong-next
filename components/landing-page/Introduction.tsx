5ce55856a25502b06bff0443181bc2f4edefb1ff
Tran Viet Anh <tran@vietanh.cz>
2023-03-21 18:04:32 +0100

    Add pagination and more styles to the slider

--- components/landing-page/Introduction.tsx
+++ components/landing-page/Introduction.tsx
@@ -1,7 +1,9 @@
 import { Container } from '@mui/material'
 import { DocumentData } from 'firebase/firestore'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Pagination } from 'swiper'
 import 'swiper/swiper.css'
+import 'swiper/css/pagination'
 import ReactHtmlParser from 'react-html-parser'
 import { Heading } from './Heading'
 import styles from './Introduction.module.scss'
@@ -14,10 +16,15 @@ export default function Introduction ({ slidesData }: Props) {
   return (
     <>
       <Heading text='Kdo jsme'/>
-      <Swiper style={{ height: '600px' }}>
-        {
-          slidesData && slidesData.map(slideData => (
-            <SwiperSlide style={{ height: '600' }} key={slideData.id}>
+      <Swiper
+        className={styles.slider}
+        modules={[Autoplay, Pagination]}
+        loop={true}
+        autoplay={{delay: 15000}}
+        pagination={{ clickable: true }}
+      >
+        {slidesData && slidesData.map(slideData => (
+            <SwiperSlide className={styles.slide} key={slideData.id}>
               <Container className={styles.textContainer}>
                 <h1>{ReactHtmlParser(slideData.title)}</h1>
                 <p>{ReactHtmlParser(slideData.text)}</p>
@@ -28,8 +35,7 @@ export default function Introduction ({ slidesData }: Props) {
                 alt={slideData.title}
               />
             </SwiperSlide>
-          ))
-        }
+          ))}
       </Swiper>
     </>
   )

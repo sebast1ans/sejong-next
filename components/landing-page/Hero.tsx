f7187b4b3a35743b03f52c101feef783364f3b4e
Tran Viet Anh <tran@vietanh.cz>
2023-03-21 01:29:53 +0100

    Create <Hero />

--- components/landing-page/Hero.tsx
+++ components/landing-page/Hero.tsx
@@ -1,17 +1,35 @@
-import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, EffectFade } from 'swiper'
 import styles from './Hero.module.scss'
 import 'swiper/swiper.css'
+import 'swiper/css/effect-fade'
 
-export default function Hero() {
+interface Props {
+  heroImages: string[]
+}
+export default function Hero ({ heroImages }: Props) {
+
   return (
-    <Swiper
-      slidesPerView={1}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-    </Swiper>
+    <>
+      <Swiper
+        modules={[Autoplay, EffectFade]}
+        autoplay={{delay: 3000}}
+        loop={true}
+        effect={'fade'}
+        speed={1000}
+        allowTouchMove={false}
+      >
+        {heroImages.map(heroImage => (
+          <SwiperSlide key={heroImage}>
+            <div
+              style={{background: `url(${heroImage}) no-repeat center`}}
+              className={styles.background}
+            >
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   )
 }
+

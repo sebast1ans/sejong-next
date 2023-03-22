f8e4a173444ada44eb18b0103b516a61e1ce5469
Tran Viet Anh <tran@vietanh.cz>
2023-03-22 16:26:06 +0100

    Add some TODOs

--- components/landing-page/Heading.tsx
+++ components/landing-page/Heading.tsx
@@ -3,6 +3,7 @@ import styles from './Heading.module.scss'
 interface Props {
   text: string
 }
+// TODO Maybe move this to a 'features' folder
 export const Heading = ({ text }: Props) => (
   <>
     <h1 className={styles.heading}>{text}</h1>

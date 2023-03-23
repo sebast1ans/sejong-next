f17c4f38dbc67fd54089bd869b327bb22edf1367
Tran Viet Anh <tran@vietanh.cz>
2023-03-23 16:24:39 +0100

    Add navigation (wip)

--- components/landing-page/Navigation.tsx
+++ components/landing-page/Navigation.tsx
@@ -1,11 +1,26 @@
-import { useTranslation } from 'next-i18next'
+import { Container } from '@mui/material'
+import Image from 'next/image'
+import logo from '../../public/logos/whiteLogo.svg'
+import styles from './Navigation.module.scss'
 
 export function Navigation() {
-  const { t } = useTranslation('navigation')
-
   return (
     <>
-      <h1>{ t('hello')}</h1>
+      <nav className={styles.navigation}>
+        <Container className={styles.container}>
+          <Image src={logo} className={styles.logo} alt='Navigation logo'/>
+          <ul>
+            <li>Aktuality</li>
+            <li>Kdo jsme</li>
+            <li>Trenéři</li>
+            <li>Historie</li>
+            <li>O Taekwondo</li>
+            <li>Tréninky</li>
+            <li>Ceník</li>
+            <li>Kontakt</li>
+          </ul>
+        </Container>
+      </nav>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,9 @@ export default function Home ({ heroImageURLs, introductionData }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.png"/>
       </Head>
+      <Navigation />
       <Hero heroImages={heroImageURLs}/>
       <Introduction slidesData={introductionData}/>
-      <Navigation />
     </>
   )
 }

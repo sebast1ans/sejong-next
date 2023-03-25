f780cec1f6339db45723bab223740cf950848d2d
Tran Viet Anh <tran@vietanh.cz>
2023-03-25 10:18:03 +0100

    Add new font Saira Semi Condensed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Saira_Semi_Condensed} from '@next/font/google'
+import '../styles/globals.scss'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const encodeSans = Saira_Semi_Condensed({
+  weight: ['400', '800'],
+  subsets: ['latin', 'vietnamese']
+})
+
+export default function App ({ Component, pageProps }: AppProps) {
+  return (
+    <main className={encodeSans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }

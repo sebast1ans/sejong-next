145e4b03578fe1e5195636c443ca8690b199d84f
Tran Viet Anh <tran@vietanh.cz>
2023-03-25 14:20:44 +0100

    Move <Navigation /> to components/layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
-import Navigation from '../components/landing-page/Navigation'
+import Navigation from '../components/layout/Navigation'
 import { Saira_Semi_Condensed } from '@next/font/google'
 import '../styles/globals.scss'
 

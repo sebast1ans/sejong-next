1373a7245c3b502c6ad134866cba530a81f388c8
Tran Viet Anh <tran@vietanh.cz>
2023-03-25 09:54:11 +0100

    Add first translations for navigation

--- components/landing-page/Navigation.tsx
+++ components/landing-page/Navigation.tsx
@@ -1,23 +1,26 @@
 import { Container } from '@mui/material'
+import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
 import logo from '../../public/logos/whiteLogo.svg'
 import styles from './Navigation.module.scss'
 
 export function Navigation() {
+  const { t } = useTranslation('navigation')
+
   return (
     <>
       <nav className={styles.navigation}>
         <Container className={styles.container}>
           <Image src={logo} className={styles.logo} alt='Navigation logo'/>
           <ul>
-            <li>Aktuality</li>
-            <li>Kdo jsme</li>
-            <li>Trenéři</li>
-            <li>Historie</li>
-            <li>O Taekwondo</li>
-            <li>Tréninky</li>
-            <li>Ceník</li>
-            <li>Kontakt</li>
+            <li>{ t('news') }</li>
+            <li>{ t('about-us') }</li>
+            <li>{ t('coaches') }</li>
+            <li>{ t('history') }</li>
+            <li>{ t('about-taekwondo') }</li>
+            <li>{ t('training-sessions') }</li>
+            <li>{ t('pricelist') }</li>
+            <li>{ t('contact') }</li>
           </ul>
         </Container>
       </nav>

5eb7169f24631611f32ccff9743adaf9c69d4f58
Tran Viet Anh <tran@vietanh.cz>
2023-03-25 15:25:33 +0100

    Add more styles and animation to navigation

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from 'react'
 import { Container } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
 import logo from '../../public/logos/whiteLogo.svg'
 import styles from './Navigation.module.scss'
 
+const scrolledOverPx = (pixels: number) => {
+  const [scrolledOver, setScrolledOver] = useState(false)
+
+  const scrollHandler = () => {
+    window.scrollY > pixels
+      ? setScrolledOver(true)
+      : setScrolledOver(false)
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler)
+    return () => document.removeEventListener('scroll', scrollHandler)
+  }, [])
+
+  return scrolledOver
+}
+
 export default function Navigation() {
   const { t } = useTranslation('navigation')
 
   return (
     <>
-      <nav className={styles.navigation}>
+      <nav className={`${styles.navigation} ${scrolledOverPx(300) && styles.scrolled}`}>
         <Container className={styles.container}>
           <Image src={logo} className={styles.logo} alt='Navigation logo'/>
           <ul>

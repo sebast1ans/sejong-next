fafeea133dd5bf7185f609ad953570e5fadc9170
Tran Viet Anh <tran@vietanh.cz>
2023-03-26 11:41:59 +0200

    Create basic language selector, programatically generate navigation items

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
-import { Container } from '@mui/material'
-import { useTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
+import { Container, FormControl, MenuItem, Select } from '@mui/material'
+import { useTranslation, i18n } from 'next-i18next'
 import Image from 'next/image'
 import logo from '../../public/logos/whiteLogo.svg'
 import styles from './Navigation.module.scss'
@@ -22,8 +23,35 @@ const scrolledOverPx = (pixels: number) => {
   return scrolledOver
 }
 
+const LanguageSelector = (): JSX.Element => {
+  const router = useRouter()
+  const { pathname, asPath, locales, locale: activeLocale } = router
+  const languageNames = new Intl.DisplayNames(['cs'], {type: 'language'})
+
+  const changeLanguageHandler = (language: string) => {
+    void router.push(pathname, asPath, { locale: language })
+  }
+
+  return (
+    <FormControl>
+      <Select
+        id='languageSelector'
+        value={activeLocale}
+        onChange={e => changeLanguageHandler(e.target.value as string)}
+      >
+        {locales && locales.map(language => (
+          <MenuItem key={language} value={language}>
+            {languageNames.of(language)}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  )
+}
+
 export default function Navigation() {
   const { t } = useTranslation('navigation')
+  const navigationKeys = i18n?.getResourceBundle('cz', 'navigation')
 
   return (
     <>
@@ -31,15 +59,11 @@ export default function Navigation() {
         <Container className={styles.container}>
           <Image src={logo} className={styles.logo} alt='Navigation logo'/>
           <ul>
-            <li>{ t('news') }</li>
-            <li>{ t('about-us') }</li>
-            <li>{ t('coaches') }</li>
-            <li>{ t('history') }</li>
-            <li>{ t('about-taekwondo') }</li>
-            <li>{ t('training-sessions') }</li>
-            <li>{ t('price-list') }</li>
-            <li>{ t('contact') }</li>
+            {Object.keys(navigationKeys).map((key: string) => (
+              <li>{t(key)}</li>
+            ))}
           </ul>
+          <LanguageSelector />
         </Container>
       </nav>
     </>

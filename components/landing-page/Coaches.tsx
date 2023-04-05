652fabc0ccf56e3e23221db311a898b1107b0141
Tran Viet Anh <tran@vietanh.cz>
2023-04-05 23:36:07 +0200

    wip

--- components/landing-page/Coaches.tsx
+++ components/landing-page/Coaches.tsx
@@ -98,10 +98,10 @@ export default function Coaches({data}: Props) {
             return (
               <Card className={styles.coach} key={coach.id}>
                 <CardContent sx={{width: '100%'}}>
-                  {/*<Paper className={styles.avatar} elevation={6}>*/}
-                  {/*  <Image src={coach.imageURL} alt={coach.name} fill sizes={'16rem'}/>*/}
-                  {/*</Paper>*/}
-                  {/*<h3>{coach.name}</h3>*/}
+                  <Paper className={styles.avatar} elevation={6}>
+                    <Image src={coach.imageURL} alt={coach.name} fill sizes={'16rem'}/>
+                  </Paper>
+                  <h3>{coach.name}</h3>
                   {/*<em>{coach.subtitle}</em>*/}
                   {/*<div>{convertHtmlToReact(coach.cardText)}</div>*/}
                 </CardContent>

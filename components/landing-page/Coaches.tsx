77d43deac4cb901f4dcf5f6e17e5d1acbff8627e
Tran Viet Anh <tran@vietanh.cz>
2023-04-05 20:03:26 +0200

    Create dialog

--- components/landing-page/Coaches.tsx
+++ components/landing-page/Coaches.tsx
@@ -2,9 +2,74 @@ import styles from './Coaches.module.scss'
 import { DocumentData } from 'firebase/firestore'
 import { Heading } from './Heading'
 import { useTranslation } from 'next-i18next'
-import { Button, Card, CardContent, Container, Paper } from '@mui/material'
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Paper
+} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
 import convertHtmlToReact from '@hedgedoc/html-to-react'
 import Image from 'next/image'
+import { useState } from 'react'
+
+type CoachDialogData = {
+  name: string,
+  subtitle: string,
+  image: string,
+  details: string
+}
+
+interface CoachDetailsDialogProps {
+  open: boolean,
+  onClose: () => void
+  coachData?: CoachDialogData
+}
+
+const CoachDetailsDialog = ({open, onClose, coachData}: CoachDetailsDialogProps) => {
+  const handleClose = () => {
+    onClose()
+  }
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      {coachData &&
+        <>
+          <DialogTitle className={styles.dialogTitle}>
+            <span className={styles.dialogAvatar}>
+              <Image
+                src={coachData.image}
+                className={styles.dialogImage}
+                fill
+                sizes={'100px'}
+                alt={coachData.name}
+              />
+            </span>
+            <div>
+              <div>{coachData.name}</div>
+              <small><em>{coachData.subtitle}</em></small>
+            </div>
+            <IconButton
+              aria-label="close"
+              onClick={onClose}
+              sx={{marginLeft: 'auto'}}
+            >
+              <CloseIcon/>
+            </IconButton>
+          </DialogTitle>
+          <DialogContent dividers>
+            {convertHtmlToReact(coachData.details)}
+          </DialogContent>
+        </>
+      }
+    </Dialog>
+  )
+}
 
 interface Props {
   data: DocumentData[]
@@ -12,6 +77,18 @@ interface Props {
 
 export default function Coaches({data}: Props) {
   const {t} = useTranslation('coaches')
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [coachDialogData, setCoachDialogData] = useState<CoachDialogData>()
+
+  const handleClickOpen = ({name, subtitle, image, details}: CoachDialogData) => {
+    setCoachDialogData({name, subtitle, image, details})
+    setDialogOpen(true)
+  }
+
+  const handleClose = () => {
+    setCoachDialogData(undefined)
+    setDialogOpen(false)
+  }
 
   return (
     <section id={'coaches'}>
@@ -29,11 +106,27 @@ export default function Coaches({data}: Props) {
                   <em>{coach.subtitle}</em>
                   <p>{convertHtmlToReact(coach.cardText)}</p>
                 </CardContent>
-                <Button className={styles.button} variant={'text'}>{t('more')}</Button>
+                <Button
+                  className={styles.button}
+                  variant={'text'}
+                  onClick={() => handleClickOpen({
+                    name: coach.name,
+                    subtitle: coach.subtitle,
+                    image: coach.imageURL,
+                    details: coach.dialogText
+                  })}
+                >
+                  {t('more')}
+                </Button>
               </Card>
             )
           }
         })}
+        <CoachDetailsDialog
+          open={dialogOpen}
+          onClose={handleClose}
+          coachData={coachDialogData}
+        />
       </Container>
     </section>
   )

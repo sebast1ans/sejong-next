4d9e23e3dc763f6cf0fd17977840327fc2a5e461
Tran Viet Anh <tran@vietanh.cz>
2023-05-08 09:57:18 +0200

    Set media queries for locations

--- components/landing-page/TrainingSessions.tsx
+++ components/landing-page/TrainingSessions.tsx
@@ -4,22 +4,22 @@ import styles from './TrainingSessions.module.scss'
 import { Place, Schedule, Commute } from '@mui/icons-material'
 import { Box, Container, Grid, Typography } from '@mui/material'
 
-export default function TrainingSessions() {
-  const {t} = useTranslation(['training-sessions', 'common'])
+export default function TrainingSessions () {
+  const { t } = useTranslation(['training-sessions', 'common'])
 
   const locations = [
     {
       name: 'Horáčkova',
       address: 'Horáčkova 1100',
-      city: `${t('prague', {ns: 'common'})} 4 - Pankrác`,
+      city: `${t('prague', { ns: 'common' })} 4 - Pankrác`,
       groups: [
         {
-          name: t('children-6-9', {ns: 'common'}),
-          time: `${t('wednesday', {ns: 'common'})} 16:30 - 17:30`,
+          name: t('children-6-9', { ns: 'common' }),
+          time: `${t('wednesday', { ns: 'common' })} 16:30 - 17:30`,
         },
         {
-          name: t('children-10-15', {ns: 'common'}),
-          time: `${t('tuesday-and-thursday', {ns: 'common'})} 17:30 - 18:30`,
+          name: t('children-10-15', { ns: 'common' }),
+          time: `${t('tuesday-and-thursday', { ns: 'common' })} 17:30 - 18:30`,
         },
         {
           name: t('juniors-and-adults', { ns: 'common' }),
@@ -32,15 +32,15 @@ export default function TrainingSessions() {
     {
       name: 'Smolkova',
       address: 'Smolkova 565/8',
-      city: `${t('prague', {ns: 'common'})} 12 - Kamýk`,
+      city: `${t('prague', { ns: 'common' })} 12 - Kamýk`,
       groups: [
         {
-          name: t('children-6-9', {ns: 'common'}),
-          time: `${t('friday', {ns: 'common'})} 15:00 - 16:00`,
+          name: t('children-6-9', { ns: 'common' }),
+          time: `${t('friday', { ns: 'common' })} 15:00 - 16:00`,
         },
         {
-          name: t('youth-and-adults', {ns: 'common'}),
-          time: `${t('wednesday', {ns: 'common'})} 18:30 - 20:00`,
+          name: t('youth-and-adults', { ns: 'common' }),
+          time: `${t('wednesday', { ns: 'common' })} 18:30 - 20:00`,
         }
       ],
       bus: ['Sídliště Libuš (165)', 'Pavlíkova (197, 215)']
@@ -48,55 +48,57 @@ export default function TrainingSessions() {
   ]
 
   return (
-    <section id={'training-sessions'}>
+    <section id={'training-sessions'} className={styles.trainingSessions}>
       <Heading text={t('training-sessions')}/>
-      <Container>
-        <Grid container>
-          {locations.map(location => (
-            <Grid item sm={12} md={6} key={location.name} className={styles.location}>
-              <Place className={`${styles.icon} ${styles.iconMap}`}/>
-              <div className={styles.address}>
-                <Typography
-                  variant={'h2'}
-                  className={styles.name}
-                >
-                  {`${t('primary-school', { ns: 'common' })} ${location.name}`}
-                </Typography>
-                <Typography variant={'body1'} sx={{fontWeight: '600'}}>{location.address}</Typography>
-                <Typography variant={'body1'}>{location.city}</Typography>
-              </div>
-              <Schedule className={`${styles.icon} ${styles.iconClock}`}/>
-              <div className={styles.time}>
-                {location.groups.map(group => (
-                  <Box sx={{ padding: '.5rem 0' }} key={group.name}>
-                    <Typography variant={'body1'} sx={{fontWeight: '600'}}>{group.name}</Typography>
-                    <Typography variant={'body1'}>{group.time}</Typography>
-                  </Box>
-                ))}
-              </div>
-              <Commute className={`${styles.icon} ${styles.iconTransport}`}/>
-              <div className={styles.transport}>
-                {location.metro && (
-                  <Box sx={{ padding: '.5rem 0' }}>
-                    <Typography variant={'body1'} sx={{fontWeight: '600'}}>Metro:</Typography>
-                    {location.metro.map(station => (
-                      <Typography variant={'body1'} key={station}>{station}</Typography>
-                    ))}
-                  </Box>)}
-                {location.bus && (
-                  <Box sx={{ padding: '.5rem 0' }}>
-                    <Typography variant={'body1'} sx={{fontWeight: '600'}}>Bus:</Typography>
-                    {location.bus.map(station => (
-                      <Typography variant={'body1'} key={station}>{station}</Typography>
-                    ))}
-                  </Box>
-                )}
-              </div>
-              <div className={styles.map}></div>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      <div className={styles.greyBackground}>
+        <Container>
+          <Grid container spacing={1}>
+            {locations.map(location => (
+              <Grid item xs={12} sm={12} md={6} lg={6} xl={6} key={location.name} className={styles.location}>
+                <Place className={`${styles.icon} ${styles.iconMap}`}/>
+                <div className={styles.address}>
+                  <Typography
+                    variant={'h2'}
+                    className={styles.name}
+                  >
+                    {`${t('primary-school', { ns: 'common' })} ${location.name}`}
+                  </Typography>
+                  <Typography variant={'body1'} sx={{ fontWeight: '600' }}>{location.address}</Typography>
+                  <Typography variant={'body1'}>{location.city}</Typography>
+                </div>
+                <Schedule className={`${styles.icon} ${styles.iconClock}`}/>
+                <div className={styles.time}>
+                  {location.groups.map(group => (
+                    <Box sx={{ padding: '.5rem 0' }} key={group.name}>
+                      <Typography variant={'body1'} sx={{ fontWeight: '600' }}>{group.name}</Typography>
+                      <Typography variant={'body1'}>{group.time}</Typography>
+                    </Box>
+                  ))}
+                </div>
+                <Commute className={`${styles.icon} ${styles.iconTransport}`}/>
+                <div className={styles.transport}>
+                  {location.metro && (
+                    <Box sx={{ padding: '.5rem 0' }}>
+                      <Typography variant={'body1'} sx={{ fontWeight: '600' }}>Metro:</Typography>
+                      {location.metro.map(station => (
+                        <Typography variant={'body1'} key={station}>{station}</Typography>
+                      ))}
+                    </Box>)}
+                  {location.bus && (
+                    <Box sx={{ padding: '.5rem 0' }}>
+                      <Typography variant={'body1'} sx={{ fontWeight: '600' }}>Bus:</Typography>
+                      {location.bus.map(station => (
+                        <Typography variant={'body1'} key={station}>{station}</Typography>
+                      ))}
+                    </Box>
+                  )}
+                </div>
+                <div className={styles.map}></div>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </div>
     </section>
   )
 }

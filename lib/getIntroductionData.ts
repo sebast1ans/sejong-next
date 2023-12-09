77a936cc80a09ad20037ab028f646fe49a802e89
Tran Viet Anh <tran@vietanh.cz>
2023-12-09 15:33:23 +0100

    Order Introduction data

--- lib/getIntroductionData.ts
+++ lib/getIntroductionData.ts
@@ -1,11 +1,17 @@
-import { collection, DocumentData, getDocs } from 'firebase/firestore'
+import { collection, DocumentData, getDocs, orderBy, query } from 'firebase/firestore'
 import { db } from './firebase'
 
 export const getIntroductionData = async () => {
   let introductionData: DocumentData = []
 
   try {
-    const querySnapshot = await getDocs(collection(db, 'who-are-we'))
+    const querySnapshot = await getDocs(
+      query(
+        collection(db, 'who-are-we'),
+        orderBy("order", "asc")
+      )
+    )
+
     querySnapshot.forEach(doc => {
       introductionData.push({ id: doc.id, ...doc.data() })
     })
